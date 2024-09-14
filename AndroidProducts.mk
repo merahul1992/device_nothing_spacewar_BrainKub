f81ed71b665a5e1e23e3d016b70c3f97f5e7fee4
merahul1992 <90108415+merahul1992@users.noreply.github.com>
2024-09-14 08:15:16 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Spacewar.mk
+    $(LOCAL_DIR)/voltage_Spacewar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Spacewar-userdebug \
-    lineage_Spacewar-user \
-    lineage_Spacewar-eng
+    voltage_Spacewar-userdebug \
+    voltage_Spacewar-user \
+    voltage_Spacewar-eng
